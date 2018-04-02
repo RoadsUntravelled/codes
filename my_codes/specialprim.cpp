d11084c858ace168e158d9cd41440e4b6531f29c
wangxufeng <997245606@qq.com>
2018-04-02 21:06:41 +0800

    crawl

---
+++ my_codes/specialprim.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: specialprim.cpp
+	> Author: 魍魉楼主
+	> Mail: wangxufeng1997@outlook.com
+	> Created Time: 2018年03月31日 星期六 19时52分30秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+bool isprime(int n){
+    
+}
+int main(){
+    int n;
+    cout<<52/6;
+    return 0;
+    
+}
